e148a2400d7fa052a5f8513ff8964d0648ba4c35
misaza311 <morris110380@gmail.com>
2022-01-20 14:39:28 -0500

    Primer commit

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+// import { from } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
+import { Global } from './global';
+import { identifierModuleUrl } from '@angular/compiler';
+
+@Injectable()
+export class ProjectService{
+    public url:string;
+
+    constructor(
+        private _http: HttpClient
+    ){
+        this.url = Global.url;
+    }
+    testService(){
+        return 'Probando el servicio de Angular'
+    }
+    saveProject(project: Project): Observable<any>{
+        let params = JSON.stringify(project);
+        let headers = new HttpHeaders().set('Content-type', 'application/json');
+
+        return this._http.post(this.url+'save-project', params, {headers: headers});
+    }
+    getProjects(): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+        return this._http.get(this.url+'projects', {headers: headers});
+    }
+    getProject(id): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+        return this._http.get(this.url+'project/'+id, {headers: headers});
+    }
+    deleteProject(id): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+        return this._http.delete(this.url+'project/'+id, {headers: headers})
+    }
+    updateProject(project): Observable<any>{
+        let params = JSON.stringify(project);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+        return this._http.put(this.url+'project/'+project._id, params, {headers: headers});
+    }
+}
